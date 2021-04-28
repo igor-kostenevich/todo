610ea64a199c2ae0ea07fb7f55b7c76016dbb2ec
i-kostenevich <kostenevich9696@gmail.com>
2021-04-28 21:58:43 +0300

    added auto height for textarea

---
+++ src/list/list.js
@@ -0,0 +1,10 @@
+const textarea = document.querySelectorAll('textarea')
+
+textarea.forEach(el => {
+  el.style.height = el.setAttribute('style', `height: ${el.scrollHeight}px`);
+  el.addEventListener('input', () => {
+    el.style.height = 'auto';
+    el.style.height = `${el.scrollHeight}px`;
+  });
+});
+
