ee4f443b4a1b5424d15d10584d193b8942fbd56f
i-kostenevich <kostenevich9696@gmail.com>
2021-04-30 20:25:05 +0300

    added logic for delete, change and completed item

--- src/list/list.js
+++ src/list/list.js
@@ -1,10 +1,192 @@
-const textarea = document.querySelectorAll('textarea')
+const btnCreateTask = document.querySelectorAll('[data-add-task]')
+const list = document.querySelector('.list')
 
-textarea.forEach(el => {
-  el.style.height = el.setAttribute('style', `height: ${el.scrollHeight}px`);
-  el.addEventListener('input', () => {
-    el.style.height = 'auto';
-    el.style.height = `${el.scrollHeight}px`;
-  });
-});
 
+let toDoList = []
+
+function isEmptyList(){ // if empty list
+  if(toDoList.length === 0){
+    console.log('s');
+  }
+}
+
+
+if (localStorage.toDoItem) {
+  toDoList = [...JSON.parse(localStorage.toDoItem)]
+  isEmptyList()
+}
+
+
+const createToDo = () => {
+  let itemTask = {
+    text: '',
+    checked: false,
+    edit: false,
+    id: Date.now(),
+    date() {
+      const date = new Date()
+      return `${date.getDate()}`
+    },
+  }
+ 
+  return itemTask
+}
+
+const template = task => `
+  <li class="list__item item ${task.edit ? 'focus' : ''}" data-id="${task.id}">
+    <div class="item__top">
+      <div class="item__items">
+        <label>
+          <input data-item="checkbox" class="check" type="checkbox" ${task.checked ? 'checked' : ''} data-checkbox>
+          <span class="checkbox icon-checkbox"></span>
+        </label>
+        <span data-item="date" class="item__date">3 Jan 2021</span>
+      </div>
+      <div class="item__icons">
+        <span class="${task.edit ? 'icon-check' : 'icon-edit'}" data-item="edit"></span>
+        <span class="icon-cart" data-item="delete"></span>
+      </div>
+    </div>
+    <textarea rows="1" class="item__text ${task.checked ? 'done' : ''}" placeholder="enter task" ${task.edit ? '' : 'disabled'}>${task.text}</textarea>
+  </li>
+`
+
+function render(){
+  const html = toDoList.map(template).join('')
+  list.innerHTML = html
+}
+render()
+
+list.addEventListener('click', e => {
+  const elementType = e.target.dataset.item
+  if(elementType){
+    const id = +e.target.closest('.list__item').dataset.id
+    const el = e.target.closest('.list__item');
+    const task = toDoList.find(item => item.id === id)
+
+    if(elementType === 'checkbox'){
+      task.checked = !task.checked
+      const currentTextarea = el.querySelector('textarea')
+      task.checked ? currentTextarea.classList.add('done') : currentTextarea.classList.remove('done')
+      updateLocalStorage()
+    } else if (elementType === 'delete') {
+      toDoList.splice(task, 1)
+      updateLocalStorage()
+      isEmptyList() // logic for empty do to list
+      render()
+    } else if (elementType === 'edit'){
+      task.edit = !task.edit
+      const currentTextarea = el.querySelector('textarea')
+      const value = currentTextarea.value
+
+      if(task.edit){
+        currentTextarea.removeAttribute('disabled')
+        e.target.classList.remove('icon-edit')
+        e.target.classList.add('icon-check')
+        el.classList.add('focus')
+        currentTextarea.focus()
+      } else {
+        currentTextarea.setAttribute('disabled', '')
+        e.target.classList.remove('icon-check')
+        e.target.classList.add('icon-edit')
+        el.classList.remove('focus')
+
+        if(!value){
+          console.log('reject');  
+        }
+      }
+
+      // ? Promise
+      updateLocalStorage()
+    }
+  }
+})
+
+const onFocusFn = (el) => {
+  return el.focus()
+}
+
+
+btnCreateTask.forEach((item) => {
+  item.addEventListener('click', () => {
+    toDoList.push(createToDo())
+    updateLocalStorage()
+    render()
+  })
+})
+
+// Update Local Storage
+const updateLocalStorage = () => {
+  localStorage.setItem('toDoItem', JSON.stringify(toDoList))
+}
+
+
+
+
+
+
+// getDate()
+// getFullYear
+// date() {
+//   const date = new Date()
+//   // return `${date.getDate()}`
+//   return 'dada'
+// }
+
+
+// btnCreateTask.forEach((item) => {
+//   item.addEventListener('click', () => {
+//     const newToDo = createToDo()
+//     toDoList.push(newToDo)
+//     localStorage.setItem('toDoItem', JSON.stringify(toDoList))
+//     render()
+
+//   })
+// })
+
+// Auto height for textarea
+const autoHeight = () => {
+  const textarea = document.querySelectorAll('textarea')
+
+  textarea.forEach((el) => {
+    el.style.height = el.setAttribute('style', `height: ${el.scrollHeight}px`)
+    el.addEventListener('input', () => {
+      el.style.height = 'auto'
+      el.style.height = `${el.scrollHeight}px`
+    })
+  })
+}
+// autoHeight()
+
+// if(localStorage.getItem('toDoItem')) {
+//   toDoList = JSON.parse(localStorage.getItem('toDoItem'))
+//   render()
+// }
+
+// function render(todoItem){
+//   let element = ''
+
+//   toDoList.forEach((item, idx) => {
+//     console.log(item);
+//     element += `
+//     <li class="list__item item">
+//       <div class="item__top">
+//         <div class="item__items">
+//           <label>
+//             <input class="check" type="checkbox" id="${idx}" ${item.checked ? 'checked' : ''}>
+//             <span class="checkbox icon-checkbox"></span>
+//           </label>
+//           <span class="item__date">3 Jan 2021</span>
+//         </div>
+//         <div class="item__icons">
+//           <span class="icon-edit"></span>
+//           <span class="icon-cart"></span>
+//         </div>
+//       </div>
+//       <textarea rows="1" class="item__text">Create main page of todolistfor frontend test task</textarea>
+//     </li>
+//     `
+//     list.innerHTML = element
+//     autoHeight()
+//   })
+// }
